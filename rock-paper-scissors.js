a7f11788ba0367da98a9ebd1f01c1215446ac07d
Xiaolu Tan <xiaolu.tan2025@outlook.com>
2025-11-10 16:40:54 +0000

    Feat: add playGame function - move playRound function inside  playGame function - change variables declaration from const to let to allow reassign humanSelection and computerSelection - play 5 rounds by reassigning the humanChoice and computerChoice and calling playRound 5 times

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,35 +19,60 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log(`It is a tie! You both chose ${computerChoice}!`);
-    }
-    else if (
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "paper" && computerChoice === "rock") ||
-        (humanChoice == "scissors" && computerChoice === "paper")
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
-        humanScore++;
-    }
-    else if (
-        (humanChoice === "rock" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "rock")
-    ) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
-        computerScore++;
-    }
-    else {
-        console.log("Invaid input! Please choose rock, paper or scissors.")
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`It is a tie! You both chose ${computerChoice}!`);
+        }
+        else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice == "scissors" && computerChoice === "paper")
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+        }
+        else if (
+            (humanChoice === "rock" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "rock")
+        ) {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+        }
+        else {
+            console.log("Invaid input! Please choose rock, paper or scissors.")
 
+        }
     }
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`Your score: ${humanScore}, Computer Score: ${computerScore}`)
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`Your score: ${humanScore}, Computer Score: ${computerScore}`)
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`Your score: ${humanScore}, Computer Score: ${computerScore}`)
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`Your score: ${humanScore}, Computer Score: ${computerScore}`)
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`Your score: ${humanScore}, Computer Score: ${computerScore}`)
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
-console.log(`Your scores: ${humanScore}, Computer Score: ${computerScore}`);
+playGame();
+console.log(`Your final scores: ${humanScore}, Computer final Score: ${computerScore}`);
 
 
